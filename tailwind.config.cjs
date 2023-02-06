c6776002a34b1cd4a5ba92974e6a9c8d79665368
Andrew Emelianenko <emelianenko.web@gmail.com>
2023-02-06 13:18:19 -0500

    Add more information to home page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       fontFamily: {
-        heading: ["Nunito", "sans-serif"],
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+        serif: ["Bitter", ...defaultTheme.fontFamily.serif],
       },
     },
   },
